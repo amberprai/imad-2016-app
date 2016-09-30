5b5f353bef12e57edb66600492090a2edc5665b0
amberprai <ambarrai7@gmail.com>
2016-09-30 23:31:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ article-one',function(req,res){
+app.get('/article-one',function(req,res){
     res.send('ARTICLE 1  ARE REQUESTED AND WILL BE SERVED HERE');
     
 });
-app.get('/ article-two',function(req,res){
+app.get('/article-two',function(req,res){
     res.send('ARTICLE 2  ARE REQUESTED AND WILL BE SERVED HERE');
     
 });
-app.get('/ article-three',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('ARTICLE 3  ARE REQUESTED AND WILL BE SERVED HERE');
     
 });
