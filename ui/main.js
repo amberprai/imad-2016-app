df2fcf3defe7abe21dc623e6d585938cd7f61f2b
amberprai <ambarrai7@gmail.com>
2016-10-06 20:22:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,6 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-
-element.innerHTML='New value';
-
-var img =document.getElementById('madi');
-img.onclick=function(){
-    img.style.marginLeft='100px';
+var button=document.getElementById('counter');
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
